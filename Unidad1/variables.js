ee4dee851d22509aa990369241d34487926784d3
asuarez-aksr <alensuarezr@est.uepmrmp.edu.ec>
2026-05-21 18:55:08 -0500

    Add files via upload

---
+++ Unidad1/variables.js
@@ -0,0 +1,6 @@
+// let (variable que no cambia), const (variable que no cambia) y var (lo mismo de ambas)
+let nombre = "Debrahy"
+console.log(nombre) //sirve para imprimir por consola
+const value1 = 1 // no se puede cambiar su valor en el futuro
+console.log(value1)
+value1 = 2
